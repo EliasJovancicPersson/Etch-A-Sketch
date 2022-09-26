7f7c523550ddd1e402dbbddde4206cb53c0a47a2
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-09-26 16:48:09 +0200

    grid aligned and not hardcoded

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const container = document.querySelector(".container");
 const dimensions = 16;
 let boxSize = 20;
+let gTCS = "";                  //grid-template-columns string
 
 
-for (let i = 0; i < Math.pow(dimensions,2); i++) {
+for (let i = 0; i < Math.pow(dimensions,2); i++) {//create grid with dimensions and box size, add border and color grey
     const element = document.createElement('div');
     element.style.width = boxSize+"px"
     element.style.height = boxSize+"px"; 
@@ -12,6 +13,8 @@ for (let i = 0; i < Math.pow(dimensions,2); i++) {
     container.appendChild(element);
 }
 
-for (let i = 0; i < dimensions; i++) {
-    
+for (let i = 0; i < dimensions; i++) {//for loop with dimensions to add column css property the right amount of times
+    gTCS += boxSize + "px ";
 }
+
+container.style.gridTemplateColumns = gTCS;
