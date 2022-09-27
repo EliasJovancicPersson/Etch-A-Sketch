1247096e237cdbd334fd2d13dac076cdc1b46533
Elias Jovancic Persson <Eliasjovancicpersson@gmail.com>
2022-09-28 00:50:17 +0200

    Size fixed with new way to calc border

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const container = document.querySelector(".container");
-const dimensions = 16;
+const sizeBtn = document.querySelector('#btnContainer .sizeBtn');
+let dimensions = 16;
 let boxSize = 40;
 let gTCS = "";                  //grid-template-columns string
 
+CreateGrid();
 
-for (let i = 0; i < Math.pow(dimensions,2); i++) {//create grid with dimensions and box size, add border and color grey
-    const element = document.createElement('div');
-    element.style.width = boxSize+"px"
-    element.style.height = boxSize+"px"; 
-    element.style.borderStyle = "solid";
-    element.style.color = "grey";
-    element.style.boxSizing = "border-box";
-    element.addEventListener('mouseover', function(){
-        changeColor(element);
-    })
-    container.appendChild(element);
+function CreateGrid(){
+    gTCS = "";
+    
+    for (let i = 0; i < Math.pow(dimensions,2); i++) {//create grid with dimensions and box size, add border and color grey
+        const element = document.createElement('div');
+        element.style.width = "auto"
+        element.style.height = "auto"; 
+        element.style.borderStyle = "solid";
+        element.style.color = "grey";
+        element.style.boxSizing = "border-box";
+        element.style.borderWidth = dimensions/(dimensions - 12) + "px";
+        element.setAttribute('class','gridElement')
+        element.addEventListener('mouseover', function(){
+            changeColor(element);
+        })
+        container.appendChild(element);
+    }
+
+    for (let i = 0; i < dimensions; i++) {//for loop with dimensions to add column css property the right amount of times
+        gTCS += "auto ";
+    }
+
+    container.style.gridTemplateColumns = gTCS;}
+
+function RemoveGrid(){
+    let gridElements = document.querySelectorAll('.gridElement');
+    gridElements.forEach(element => {
+        element.remove();
+    });
 }
 
-for (let i = 0; i < dimensions; i++) {//for loop with dimensions to add column css property the right amount of times
-    gTCS += boxSize + "px ";
-}
-
-container.style.gridTemplateColumns = gTCS;
 
 
 function changeColor(element){
 element.style.backgroundColor = "red";
 }
+
+sizeBtn.addEventListener('click',function(){        //prompt for changing size, create grid and remove grid
+    dimensions = window.prompt("Enter new size of grid");
+    RemoveGrid();
+    CreateGrid();
+})
